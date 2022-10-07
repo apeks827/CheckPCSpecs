3ab2af6dae514359c3ee20c6aa3c134d8cfd135f
apeks827 <apeks827@ya.ru>
2022-10-07 18:15:14 +0300

    обернул в функции исправил пути для сборки в exe add debug cleanup code

--- Windowed.py
+++ Windowed.py
@@ -1,198 +1,217 @@
-import tkinter as tk
+import sys
 from tkinter import *
+import os
 import string
 import subprocess
-
 import psutil
 import win32com.client
 import wmi
 import platform
 import cpuinfo
 import speedtest
-import out_color
 import ram_gb
-from ping3 import ping, verbose_ping
+from ping3 import ping
 import tkinter as tk
-from contextlib import redirect_stdout
+from PIL import ImageTk
 
-pc_score = 0
+end_score = 0
 
-bad_cpus = ['atom', 'duo', 'quad', 'dual-core', 'sempron']
+
+def pc_score(score):
+    global end_score
+    end_score += score
+
+
+print("Работаем...")
 
 # Operating system info
-if platform.release() == "10" or platform.release() == "11":
-    print(f"Версия ОС: {out_color.out_green(platform.release())}")
-    out_color.clear_color()
-    pc_score += 2
-    os_result = 1
-elif platform.release() == "8" or platform.release() == "8.1":
-    print(f"Версия ОС: {out_color.out_yellow(platform.release())}")
-    out_color.clear_color()
-    pc_score += 1
-    os_result = 0
-else:
-    print(f"Версия ОС: {out_color.out_red(platform.release())}")
-    out_color.clear_color()
-    pc_score -= 1
-    os_result = -1
+def os_info():
+    os_platform = platform.release()
 
-# Machine type
-if platform.machine() == "AMD64":
-    print(f"Разрядность ОС: {out_color.out_green('x64')}")
-    out_color.clear_color()
-    pc_score += 1
-    platform_result = 1
-else:
-    print(f"Разрядность ОС: {out_color.out_red('x32')}")
-    out_color.clear_color()
-    platform_result = -1
-    pc_score -= 999
 
-# HARDWARE
+    if os_platform == "10" or os_platform == "11":
+
+        result = 2
+        os_result = 1
+    elif os_platform == "8" or os_platform == "8.1":
+        #print(f"Версия ОС: {os_platform}")
+        result = 1
+        os_result = 0
+    else:
+        #print(f"Версия ОС: {os_platform}")
+        result = -1
+        os_result = -1
+    return pc_score(result), os_result, os_platform
+
+
+# Arch
+def arch_test():
+    if platform.machine() == "AMD64":
+        #print(f"Разрядность ОС: x64")
+        result = 1
+        platform_result = 1
+    else:
+        #print(f"Разрядность ОС: x32")
+        platform_result = -1
+        result = -999
+    return pc_score(result), platform_result
+
+
 # Memory
-if ram_gb.ram_specs() >= 7.8:
-    ram = print(f"Количество RAM: {out_color.out_green(ram_gb.ram_specs())} Gb")
-    out_color.clear_color()
-    pc_score += 5
-    ram_result = 1
-elif ram_gb.ram_specs() >= 5.8:
-    print(f"Количество RAM: {out_color.out_yellow(ram_gb.ram_specs())} Gb")
-    out_color.clear_color()
-    pc_score += 2
-    ram_result = 0
-else:
-    print(f"Количество RAM: {out_color.out_red(ram_gb.ram_specs())} Gb")
-    out_color.clear_color()
-    pc_score -= 999
-    ram_result = -1
+def memory():
+    if ram_gb.ram_specs() >= 7.8:
+        #print(f"Количество RAM: {ram_gb.ram_specs()} Gb")
+        result = 5
+        ram_result = 1
+    elif ram_gb.ram_specs() >= 5.8:
+        #print(f"Количество RAM: {ram_gb.ram_specs()} Gb")
+        result = 2
+        ram_result = 0
+    else:
+        #print(f"Количество RAM: {ram_gb.ram_specs()} Gb")
+        result = -999
+        ram_result = -1
+    return pc_score(result), ram_result
+
 
 # CPU
-cpu = cpuinfo.get_cpu_info()['brand_raw']
-cores = psutil.cpu_count(logical=False)
-threads = psutil.cpu_count(logical=True)
-fine_name_cpu = cpu.lower().translate(str.maketrans('', '', string.punctuation)).replace(' ', '')
-if [a for a in bad_cpus if a in fine_name_cpu]:
-    print(f"CPU: {out_color.out_red(cpu)}")
-    out_color.clear_color()
-    pc_score -= 999
-    cpu_result = -1
-else:
-    if cores >= 4 and threads >= 4:
-        print(f"CPU: {out_color.out_green(cpu)}")
-        out_color.clear_color()
-        pc_score += 5
-        cpu_result = 1
-    elif cores >= 2 and threads >= 4:
-        print(f"CPU: {out_color.out_yellow(cpu)}")
-        out_color.clear_color()
-        pc_score += 2
-        cpu_result = 0
-    else:
-        print(f"CPU: {out_color.out_red(cpu)}")
-        out_color.clear_color()
-        pc_score -= 999
+def cpu():
+    bad_cpus = ['atom', 'duo', 'quad', 'dual-core', 'sempron']
+    cpu = cpuinfo.get_cpu_info()['brand_raw']
+    cores = psutil.cpu_count(logical=False)
+    threads = psutil.cpu_count(logical=True)
+    fine_name_cpu = cpu.lower().translate(str.maketrans('', '', string.punctuation)).replace(' ', '')
+    if [a for a in bad_cpus if a in fine_name_cpu]:
+        #print(f"CPU: {cpu}")
+        result = -999
         cpu_result = -1
-# Disk
-wmiobj = win32com.client.GetObject("winmgmts:Win32_LogicalDisk.DeviceID='C:'")
-c_drive = wmi._wmi_object(wmiobj)
+    else:
+        if cores >= 4 and threads >= 4:
+            #print(f"CPU: {cpu}")
 
-DiskType = subprocess.run(["powershell", "-Command", "Get-PhysicalDisk | ft -AutoSize MediaType"],
-                          capture_output=True)
-DiskType = str(DiskType)
-if 'SSD' in DiskType:
-    print(f"Тип диска: {out_color.out_green('SSD')}")
-    out_color.clear_color()
-    pc_score += 5
-    disk_result = 1
-elif ram_gb.ram_specs() >= 7.8:
-    print(out_color.out_yellow("HDD or eMMC"))
-    disk_result = 0
-else:
-    print(out_color.out_red("HDD or eMMC"))
-    pc_score -= 999
-    disk_result = -1
+            result = 5
+            cpu_result = 1
+        elif cores >= 2 and threads >= 4:
+            #print(f"CPU: {cpu}")
+
+            result = 2
+            cpu_result = 0
+        else:
+            #print(f"CPU: {cpu}")
+
+            result = -999
+            cpu_result = -1
+    return pc_score(result), cpu_result, cpu
+
+
+# Disk
+def disk():
+    wmiobj = win32com.client.GetObject("winmgmts:Win32_LogicalDisk.DeviceID='C:'")
+    c_drive = wmi._wmi_object(wmiobj)
+
+    DiskType = subprocess.run(["powershell", "-Command", "Get-PhysicalDisk | ft -AutoSize MediaType"],
+                              capture_output=True)
+    DiskType = str(DiskType)
+    if 'SSD' in DiskType:
+        # print(f"Тип диска: SSD")
+        result = 5
+        disk_result = 1
+    elif ram_gb.ram_specs() >= 7.8:
+        # print("HDD or eMMC")
+        disk_result = 0
+    else:
+        # print("HDD or eMMC")
+        result = 999
+        disk_result = -1
+
+    return pc_score(result), disk_result
+
 
 # speedtest
-print("Тестируем скорость интернета...")
-test_ping = round(ping('ya.ru') * 1000, 2)
-#sp = speedtest.Speedtest()
-down = 100
-up = 100
-#down = round(sp.download() / (10 ** 6), 2)
-#up = round(sp.upload() / (10 ** 6), 2)
+def ethtest():
+    #print("Тестируем скорость интернета...")
+    test_ping = round(ping('ya.ru') * 1000, 2)
 
-if down >= 20 and up >= 10:
-    if test_ping <= 30:
-        print(f"Скорость загрузки: {out_color.out_green(down)} Mbps")
-        out_color.clear_color()
-        print(f"Скорость отдачи: {out_color.out_green(up)} Mbps")
-        out_color.clear_color()
-        print(f"Пинг: {out_color.out_green(test_ping)} ms")
-        out_color.clear_color()
-        pc_score += 5
-        eth_score = 5
-    elif test_ping <= 70:
-        print(f"Скорость загрузки: {out_color.out_green(down)} Mbps")
-        out_color.clear_color()
-        print(f"Скорость отдачи: {out_color.out_green(up)} Mbps")
-        out_color.clear_color()
-        print(f"Пинг: {out_color.out_yellow(test_ping)} ms")
-        out_color.clear_color()
-        pc_score += 2
-        eth_score = 4
-    else:
-        print(f"Скорость загрузки: {out_color.out_green(down)} Mbps")
-        out_color.clear_color()
-        print(f"Скорость отдачи: {out_color.out_green(up)} Mbps")
-        out_color.clear_color()
-        print(f"Пинг: {out_color.out_red(test_ping)} ms")
-        out_color.clear_color()
-        eth_score = 3
-else:
-    if test_ping <= 30:
-        print(f"Скорость загрузки: {out_color.out_red(down)} Mbps")
-        out_color.clear_color()
-        print(f"Скорость отдачи: {out_color.out_red(up)} Mbps")
-        out_color.clear_color()
-        print(f"Пинг: {out_color.out_green(test_ping)} ms")
-        out_color.clear_color()
-        pc_score += 2
-        eth_score = 2
-    elif test_ping <= 70:
-        print(f"Скорость загрузки: {out_color.out_red(down)} Mbps")
-        out_color.clear_color()
-        print(f"Скорость отдачи: {out_color.out_red(up)} Mbps")
-        out_color.clear_color()
-        print(f"Пинг: {out_color.out_yellow(test_ping)} ms")
-        out_color.clear_color()
-        pc_score += 1
-        eth_score = 1
-    else:
-        print(f"Скорость загрузки: {out_color.out_red(down)} Mbps")
-        out_color.clear_color()
-        print(f"Скорость отдачи: {out_color.out_red(up)} Mbps")
-        out_color.clear_color()
-        print(f"Пинг: {out_color.out_red(test_ping)} ms")
-        out_color.clear_color()
-        eth_score = -1
+    try:
+        sp = speedtest.Speedtest()
+        # down = 100
+        # up = 100
+        down = round(sp.download() / (10 ** 6), 2)
+        up = round(sp.upload() / (10 ** 6), 2)
+        success = 1
 
-if pc_score >= 18:
-    print(out_color.out_green("ПК соответствует требованиям"))
-    out_color.clear_color()
-elif pc_score < 0:
-    print(out_color.out_red("ПК не соответствует требованиям"))
-    out_color.clear_color()
-else:
-    print(out_color.out_yellow("Приемлемо"))
-    out_color.clear_color()
-print(f"Debug: {pc_score}")
-# screenshot = pyautogui.screenshot('C:/users/sergmakarov/Desktop/screenshot.png')
+        if down >= 20 and up >= 10:
+            if test_ping <= 30:
+                # print(f"Скорость загрузки: {down} Mbps")
+                #
+                # print(f"Скорость отдачи: {up} Mbps")
+                #
+                # print(f"Пинг: {test_ping} ms")
+
+                result = 5
+                eth_score = 5
+            elif test_ping <= 70:
+                # print(f"Скорость загрузки: {down} Mbps")
+                #
+                # print(f"Скорость отдачи: {up} Mbps")
+                #
+                # print(f"Пинг: {test_ping} ms")
+
+                result = 2
+                eth_score = 4
+            else:
+                # print(f"Скорость загрузки: {down} Mbps")
+                #
+                # print(f"Скорость отдачи: {up} Mbps")
+                #
+                # print(f"Пинг: {test_ping} ms")
+
+                eth_score = 3
+        else:
+            if test_ping <= 30:
+                # print(f"Скорость загрузки: {down} Mbps")
+                # print(f"Скорость отдачи: {up} Mbps")
+                # print(f"Пинг: {test_ping} ms")
+                result = 2
+                eth_score = 2
+            elif test_ping <= 70:
+                # print(f"Скорость загрузки: {down} Mbps")
+                # print(f"Скорость отдачи: {up} Mbps")
+                # print(f"Пинг: {test_ping} ms")
+                result = 1
+                eth_score = 1
+
+            else:
+                # print(f"Скорость загрузки: {down} Mbps")
+                # print(f"Скорость отдачи: {up} Mbps")
+                # print(f"Пинг: {test_ping} ms")
+                eth_score = -1
+
+    except:
+        # print("Невозможно определить скорость соединения")
+        success = 0
+        result = 0
+        eth_score, down, up = -999, -999, -999
+
+    return success, pc_score(result), eth_score, down, up, test_ping
+
+
+def main():
+    pass
+
+# Debug
+# print(f"Версия ОС: score, os_result, os_platform {os_info()}")
+# print(f"Разрядность ОС: pc_score(result), platform_result) {arch_test()}")
+# print(f"Память: ram_gb.ram_specs(), pc_score(result), ram_result {ram_gb.ram_specs()}, {memory()}")
+# print(f"CPU: pc_score(result), cpu_result, cpu {cpu()}")
+# print(f"Disk: pc_score(result), disk_result {disk()}")
+# print(f"speedtest: success, pc_score(result), eth_score, down, up, test_ping {speedtest}")
 
 root = Tk()
 root.title("Проверка ПК на соответствие требованиям")
 root.columnconfigure(2)
-w = 460
-h = 250
+w = 485
+h = 350
 
 sw = root.winfo_screenwidth()
 sh = root.winfo_screenheight()
@@ -200,74 +219,146 @@ sh = root.winfo_screenheight()
 x = (sw - w) / 2
 y = (sh - h) / 2
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-#Вывод ОС
-lable_os_pre = tk.Label(text="Версия ОС:")
-lable_os_pre.grid(column=0, row=0, sticky="e")
-if os_result == 1:
-    label_os_result = tk.Label(text=f"{platform.release()}", fg="green")
-elif os_result == 0:
-    label_os_result = tk.Label(text=f"{platform.release()}", fg="yellow")
-else:
-    label_os_result = tk.Label(text=f"{platform.release()}", fg="red")
-label_os_result.grid(column=1,row=0, sticky="w")
+root.resizable(0, 0)
 
-#Платформа
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+path = resource_path("icon.ico")
+path2 = resource_path("logo.png")
+root.iconbitmap(default=path)
+test = ImageTk.PhotoImage(file=path2)
+
+label1 = tk.Label(image=test)
+label1.image = test
+label1.place(x=10, y=0)
+label2 = tk.Label(text="")
+label3 = tk.Label(text="")
+label4 = tk.Label(text="")
+label5 = tk.Label(text="")
+label6 = tk.Label(text="")
+label2.grid(column=0, row=0)
+label3.grid(column=0, row=1)
+label4.grid(column=0, row=2)
+label5.grid(column=0, row=3)
+label6.grid(column=0, row=4)
+# Вывод ОС
+os = os_info()
+os_result, os_platform = os[1], os[2]
+lable_os_pre = tk.Label(text="Версия ОС:")
+lable_os_pre.grid(column=1, row=5, sticky="e")
+if os_result == 1:
+    label_os_result = tk.Label(text=f"{os_platform}", fg="green")
+elif os_result == 0:
+    label_os_result = tk.Label(text=f"{os_platform}", fg="yellow")
+elif os_result == -1:
+    label_os_result = tk.Label(text=f"{os_platform}", fg="red")
+label_os_result.grid(column=2, row=5, sticky="w")
+
+# Платформа
+arch = arch_test()
+platform_result = arch[1]
 label_arch_pre = tk.Label(text="Разрядность ОС:")
-label_arch_pre.grid(column=0, row=1, sticky="e")
+label_arch_pre.grid(column=1, row=6, sticky="e")
 if platform_result == 1:
     label_platform_result = tk.Label(text=f"x64", fg="green")
-else:
+elif platform_result == -1:
     label_platform_result = tk.Label(text=f"x32", fg="red")
-label_platform_result.grid(column=1,row=1, sticky="w")
+label_platform_result.grid(column=2, row=6, sticky="w")
 
-#Память
+# Память
+mem = memory()
+ram_result = mem[1]
 lable_ram_pre = tk.Label(text="RAM:")
-lable_ram_pre.grid(column=0, row=2, sticky="e")
+lable_ram_pre.grid(column=1, row=7, sticky="e")
 if ram_result == 1:
     label_ram_result = tk.Label(text=f"{ram_gb.ram_specs()} Gb", fg="green")
 elif ram_result == 0:
     label_ram_result = tk.Label(text=f"{ram_gb.ram_specs()} Gb", fg="yellow")
 else:
     label_ram_result = tk.Label(text=f"{ram_gb.ram_specs()} Gb", fg="red")
-label_ram_result.grid(column=1,row=2, sticky="w")
+label_ram_result.grid(column=2, row=7, sticky="w")
 
-#CPU
+# CPU
+cpu_complete_query = cpu()
+cpu_result, cpu = cpu_complete_query[1], cpu_complete_query[2]
 label_cpu = tk.Label(text="CPU:")
-label_cpu.grid(column=0, row=3, sticky="e")
+label_cpu.grid(column=1, row=8, sticky="e")
 if cpu_result == 1:
     label_cpu_result = tk.Label(text=f"{cpu}", fg="green")
 elif cpu_result == 0:
     label_cpu_result = tk.Label(text=f"{cpu}", fg="yellow")
 else:
     label_cpu_result = tk.Label(text=f"{cpu}", fg="red")
-label_cpu_result.grid(column=1,row=3, sticky="w")
+label_cpu_result.grid(column=2, row=8, sticky="w")
 
-#Disk
-#disk_result = -1
+# Disk
+# disk_result = -1
+disk_result = disk()
 label_disk = tk.Label(text="Тип диска:")
-label_disk.grid(column=0, row=4, sticky="e")
-if disk_result == 1:
+label_disk.grid(column=1, row=9, sticky="e")
+if disk_result[1] == 1:
     label_disk_result = tk.Label(text=f"SSD", fg="green")
-elif disk_result == 0:
+elif disk_result[1] == 0:
     label_disk_result = tk.Label(text=f"HDD or eMMC", fg="yellow")
 else:
     label_disk_result = tk.Label(text=f"HDD or eMMC", fg="red")
-label_disk_result.grid(column=1,row=4, sticky="w")
+label_disk_result.grid(column=2, row=9, sticky="w")
 
-#Speedtest
-label_eth = tk.Label(text="Тип диска:")
-label_eth.grid(column=0, row=5, sticky="e")
-if eth_score >= 3:
-    label_eth_result_d = tk.Label(text=f"SSD", fg="green")
-    label_eth_result_u = tk.Label(text=f"SSD", fg="green")
-    label_eth_result_d
-elif disk_result == 2 or disk_result == 1:
-    label_disk_result = tk.Label(text=f"HDD or eMMC", fg="yellow")
+# Speedtest
+speedtest_result = ethtest()
+label_eth = tk.Label(text="Скорость сети:")
+label_eth.grid(column=1, row=10, sticky="e")
+success = speedtest_result[0]
+
+if success == 1:
+    eth_score, down, up, test_ping = speedtest_result[2], speedtest_result[3], speedtest_result[4], speedtest_result[5]
+    if eth_score == 5:
+        label_eth_result_d = tk.Label(text=f"Загрузка: {down}", fg="green")
+        label_eth_result_u = tk.Label(text=f"Отдача: {up}", fg="green")
+        label_eth_result_p = tk.Label(text=f"Пинг: {test_ping}", fg="green")
+    elif eth_score == 4:
+        label_eth_result_d = tk.Label(text=f"Загрузка: {down}", fg="green")
+        label_eth_result_u = tk.Label(text=f"Отдача: {up}", fg="green")
+        label_eth_result_p = tk.Label(text=f"Пинг: {test_ping}", fg="yellow")
+    elif eth_score == 3:
+        label_eth_result_d = tk.Label(text=f"Загрузка: {down}", fg="green")
+        label_eth_result_u = tk.Label(text=f"Отдача: {up}", fg="green")
+        label_eth_result_p = tk.Label(text=f"Пинг: {test_ping}", fg="red")
+    elif eth_score == 2:
+        label_eth_result_d = tk.Label(text=f"Загрузка: {down}", fg="red")
+        label_eth_result_u = tk.Label(text=f"Отдача: {up}", fg="red")
+        label_eth_result_p = tk.Label(text=f"Пинг: {test_ping}", fg="green")
+    elif eth_score == 1:
+        label_eth_result_d = tk.Label(text=f"Загрузка: {down}", fg="red")
+        label_eth_result_u = tk.Label(text=f"Отдача: {up}", fg="red")
+        label_eth_result_p = tk.Label(text=f"Пинг: {test_ping}", fg="yellow")
+    elif eth_score == 0:
+        label_eth_result_d = tk.Label(text=f"Загрузка: {down}", fg="red")
+        label_eth_result_u = tk.Label(text=f"Отдача: {up}", fg="red")
+        label_eth_result_p = tk.Label(text=f"Пинг: {test_ping}", fg="red")
+
+    label_eth_result_d.grid(column=2, row=10, sticky="w")
+    label_eth_result_u.grid(column=2, row=11, sticky="w")
+    label_eth_result_p.grid(column=2, row=12, sticky="w")
 else:
-    label_disk_result = tk.Label(text=f"HDD or eMMC", fg="red")
-label_disk_result.grid(column=1,row=5, sticky="w")
+    label_eth_result_d = tk.Label(text=f"Скорость соединения определить невозможно", fg="red")
+    label_eth_result_d.grid(column=2, row=10, sticky="w")
 
+if end_score >= 18:
+    label_pc_result = tk.Label(text="ПК соответствует требованиям", fg="green", font=("Arial", 15))
+elif end_score < 0:
+    label_pc_result = tk.Label(text="ПК не соответствует требованиям", fg="red", font=("Arial", 15))
+else:
+    label_pc_result = tk.Label(text="Приемлемо", fg="yellow", font=("Arial", 15))
+label_pc_result.place(x=470 / 2, y=290, anchor="center")
 
 quitButton = Button(root, text="Закрыть", command=root.quit)
-quitButton.place(x=200, y=150)
+quitButton.place(x=200, y=310)
 root.mainloop()
