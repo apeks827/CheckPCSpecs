5848398da6a8695ad448560a915e19c4e1505b70
apeks827 <apeks827@ya.ru>
2022-10-07 18:17:58 +0300

    обернул в функции исправил пути для сборки в exe add debug cleanup code

---
+++ ram_gb.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+import psutil
+import variables_data
+
+
+# Assign variable to store total memory value.
+mem = psutil.virtual_memory()
+mem_total = float(mem.total/variables_data.gigabyte)
+
+# Defining function ram_specs that uses modules/functions from psutil library and from variables_data.
+def ram_specs():
+        return round(float(mem_total),2)
---
+++ variables_data.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+# Defining data measurements
+bit = 0 or 1
+byte = float(8)
+kilobyte = float(1024)
+megabyte = float(1024 * 1024)
+gigabyte = float(1024 * 1024 * 1024)
