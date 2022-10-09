fad4f9ec0adbfa8eb31afeb1fdd5acd5351c4132
apeks827 <apeks827@ya.ru>
2022-10-09 22:49:39 +0300

    add async for ethtest

--- internal/ram_gb.py
+++ internal/ram_gb.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python3
 
 import psutil
-import variables_data
+from internal import variables_data
 
 
 # Assign variable to store total memory value.
 mem = psutil.virtual_memory()
-mem_total = float(mem.total/variables_data.gigabyte)
+mem_total = float(mem.total / variables_data.gigabyte)
 
 # Defining function ram_specs that uses modules/functions from psutil library and from variables_data.
 def ram_specs():
